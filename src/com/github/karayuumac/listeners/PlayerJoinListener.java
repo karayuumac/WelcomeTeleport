4c66ba5fbe62af85290793642017f1bfa13ac5b6
karayuu <karayuumac@users.noreply.github.com>
2017-09-24 18:17:03 +0900

    テレポートうまくいかないのでちょいと消す

--- src/com/github/karayuumac/listeners/PlayerJoinListener.java
+++ src/com/github/karayuumac/listeners/PlayerJoinListener.java
@@ -5,10 +5,6 @@ import com.github.karayuumac.WelcomeTeleport;
 import com.github.karayuumac.config.Config;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.ServerConnectedEvent;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -37,28 +33,6 @@ public class PlayerJoinListener implements Listener {
                     player.sendPluginMessage(WelcomeTeleport.plugin, "BungeeCord", byteArrayDataOutput.toByteArray());
                 }
             }.runTaskLater(WelcomeTeleport.plugin, 20);
-        }
-    }
-
-    /**
-     * 上のメソッド後に指定座標に飛ばすメソッド
-     */
-    @EventHandler
-    public void playerTeleport(PlayerJoinEvent event) {
-        Bukkit.getServer().getLogger().info("playerjoinevent");
-        Player player = event.getPlayer();
-        if (player.getServer().getName().equals(config.getTeleportServer())) {
-            if (config.isAfterTeleported() && !sql.isPlayerDataExisted(player)) {
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        player.teleport(new Location(player.getWorld(), config.getTeleportX(),
-                                config.getTeleportY(), config.getTeleportZ()));
-                        Bukkit.getServer().getLogger().info("x.y.z:" + config.getTeleportX() + "," + config.getTeleportY() + "," + config.getTeleportZ());
-                    }
-                }.runTaskLater(WelcomeTeleport.plugin, 20);
-
-            }
         }
     }
 }
